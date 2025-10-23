c146fdfe0ff83ed218c26ea1e6b3ef7362b4ceb0
Juan Agudelo <verojuan9007@gmail.com>
2025-10-22 20:36:35 -0500

    gradient added on a self-closing div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,12 +26,22 @@ const Hero = () => {
   return (
     <div className="w-full h-[500px] lg:h-[600px] bg-linear-to-r from-black to-transparent">
       <div className="h-full w-full ">
-        <div className="absolute w-full h-[500px] lg:h-[600px] ">
-          <img
-            className="w-full h-full object-cover object-top "
-            src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
-            alt={title}
-          />
+        <div className="absolute w-full h-[500px] lg:h-[600px] bg-linear-to-r from-black" />
+        <img
+          className="w-full h-full object-cover object-top "
+          src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
+          alt={title}
+        />
+        <div className="absolute w-full top-[20%] lg:top-[32%]">
+          <h1>{title}</h1>
+          <div>
+            <button>Play</button>
+            <button>Watch Later</button>
+            <div>
+              <p>{release_date}</p>
+              <p>{overview}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
